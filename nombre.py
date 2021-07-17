22f870b31a491024f808a6f91413e3761177a839
CeliaRivera <86999303+CeliaRivera@users.noreply.github.com>
2021-07-17 17:47:41 -0500

    Add files via upload

---
+++ nombre.py
@@ -0,0 +1,12 @@
+# Ejercicio 4.3.1
+# Crear un archivo .py que imprima el siguiente texto: Al finalizar dijo: "Estaremos calculando el área' de las células".
+
+# Utilizamos print para que imprima el mensaje o texto
+# Para manipular la cadena de caracteres empleamos las comillas " "
+# Se utiliza \ para separar un tipo de comilla dentro de comillas del mismo tipo
+# \n separa las frases
+
+print("Al finalizar dijo: \"Estaremos calculando el área' de las células\".")
+
+print("\n Al finalizar dijo: \"Estaremos calculando el area' de las celulas\".\n")
+
